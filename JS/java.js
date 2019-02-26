ff58061b50342421dce60e97e031df4813046863
quangnguyen6889 <quangnguyenhd6889@gmail.com>
2019-02-26 19:37:23 +0700

    kikiki

--- JS/java.js
+++ JS/java.js
@@ -1,11 +1,9 @@
-function thongbao() {
-	alert("hoc js")
-}
-
-function tinhtong(x,y) {
-	alert(x+y);
-}
-
-var total = function(x,y) {
-	return x+y;
+function giaithua(n){
+	var giaiThua = 1
+	// a = (n*(n-1))
+	// console.log(a);
+	for (i = 1; i <= n; i++) {
+		giaiThua *=i
+	}
+	return giaiThua;
 }
